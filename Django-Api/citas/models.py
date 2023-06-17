29b0c09b249709fa46dfc928e9a4f6fb1abd8a31
camilo <camilovd1986@hotmail.com>
2023-06-17 00:36:52 -0500

    git

--- Django-Api/citas/models.py
+++ Django-Api/citas/models.py
@@ -14,6 +14,7 @@ class Cita(models.Model):
         BARBERIA = "BARBERIA",
         MASAJES = "MASAJES",
         PELUQUERIA = "PELUQUERIA",
+        FACIAL = "FACIAL",
         UNAS = "UNAS",
 
     nombre = models.CharField(
